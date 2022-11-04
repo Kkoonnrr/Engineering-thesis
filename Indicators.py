b5a3a4b1e6659909b8258b7d945a50650f1cd585
kkoonnrr <konrad@synowiec.eu>
2022-11-04 23:53:52 +0100

    added CCI and Ichimoku Cloud

--- Indicators.py
+++ Indicators.py
@@ -8,6 +8,8 @@ class Indicators:
         self.sma_result = self.simple_moving_average(sma_periods)
         self.bb_result = self.bollinger_bands()
         self.rsi_result = self.relative_strength_index()
+        self.cci_result = self.commodity_channel_index()
+        self.ichimoku_cloud_result = self.ichimoku_cloud()
 
     def simple_moving_average(self, sma_periods: list):
         a = list()
@@ -33,9 +35,33 @@ class Indicators:
         rsi = 100 - (100 / (1 + (ma_up / ma_down)))
         return rsi
 
-    def commodity_channel_index(self):
-        pass
+    def commodity_channel_index(self, periods=20):
+        tp = (self.data['high'] + self.data['low'] + self.data['close']) / 3
+        sma_tp = tp.rolling(periods).mean()
+        mad = tp.rolling(periods).apply(lambda x: pd.Series(x).mad())
+        cci = (tp - sma_tp) / (0.015 * mad)
+        return cci
 
     def ichimoku_cloud(self):
-        pass
+        elements = pd.DataFrame(columns=['Tenkan-sen', 'Kijun-sen', 'Senkou Span A', 'Senkou Span B', 'chikou_span'])
+        period9_high = self.data['high'].rolling(window=9).max()
+        period9_low = self.data['low'].rolling(window=9).min()
+        elements['Tenkan-sen'] = (period9_high + period9_low) / 2
+
+        period26_high = self.data['high'].rolling(window=26).max()
+        period26_low = self.data['low'].rolling(window=26).min()
+        elements['Kijun-sen'] = (period26_high + period26_low) / 2
+
+        elements['Senkou Span A'] = ((elements['Tenkan-sen'] + elements['Kijun-sen']) / 2)
+
+        period52_high = self.data['high'].rolling(window=52).max()
+        period52_low = self.data['low'].rolling(window=52).min()
+        elements['Senkou Span B'] = ((period52_high + period52_low) / 2)
+        for i in range(0,26):
+            elements.loc[len(elements)] = ['nan', 'nan', 'nan', 'nan', 'nan']
+        elements['Senkou Span B'] = elements['Senkou Span B'].shift(26)
+        elements['Senkou Span A'] = elements['Senkou Span A'].shift(26)
+
+        elements['chikou_span'] = self.data['close'].shift(-22)
+        return elements
 
--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-
 # random forest, linear regression, decisive tree, long short-term memory
 from binance import Client
 from Secret import api_key, secret_key
@@ -12,29 +11,64 @@ from plotly.subplots import make_subplots
 client = Client(api_key, secret_key)
 symbol = "BTCUSDT"
 interval = '5m'
-klines = client.get_historical_klines(symbol, interval, "23 Oct,2022")
+klines = client.get_historical_klines(symbol, interval, "03 Nov,2022")
 for line in klines:
     del line[5:]
 data = pd.DataFrame(klines)
 data.columns = ['open_time', 'open', 'high', 'low', 'close']
-data.open_time = [dt.datetime.fromtimestamp(x/1000.0) for x in data.open_time]
+data.open_time = [dt.datetime.fromtimestamp(x / 1000.0) for x in data.open_time]
+data = data.astype({"open": float, "high": float, "low": float, "close": float})
 indicators = Indicators(data, [20, 50, 100])
 
 fig = make_subplots(
-    rows=2, cols=1,
+    rows=3, cols=1,
     shared_xaxes=True,
     vertical_spacing=0.03,
-    row_heights=[0.85, 0.15]
+    row_heights=[0.80, 0.10, 0.10]
 )
-
+add_time = list()
+add_time.append(data.tail(1)['open_time'].item())
+for i in range(0, 26):
+    add_time.append(add_time[i]+dt.timedelta(minutes = 5))
+add_series = pd.Series(add_time)
+b=add_series.values.astype('datetime64[m]')
+a=data['open_time'][100:].values.astype('datetime64[m]')
+c = a.tolist()
+d = b.tolist()
+e = c+d
+f = pd.Series(e)
+fig.add_scatter(
+    x=data['open_time'][100:],
+    y=indicators.ichimoku_cloud_result['Tenkan-sen'][100:],
+    name='Tenkan-sen', row=1, col=1)
+fig.add_scatter(
+    x=data['open_time'][100:],
+    y=indicators.ichimoku_cloud_result['Kijun-sen'][100:],
+    name='Kijun-sen', row=1, col=1)
+fig.add_scatter(
+    x=f,
+    y=indicators.ichimoku_cloud_result['Senkou Span A'][100:],
+    name='Senkou Span A', row=1, col=1)
+fig.add_scatter(
+    x=f,
+    y=indicators.ichimoku_cloud_result['Senkou Span B'][100:],
+    name='Senkou Span B', row=1, col=1)
+fig.add_scatter(
+    x=data['open_time'][100:],
+    y=indicators.ichimoku_cloud_result['chikou_span'][100:],
+    name='chikou_span', row=1, col=1)
+fig.add_scatter(
+    x=data['open_time'][100:],
+    y=indicators.cci_result[100:],
+    name='CCI', row=3, col=1)
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.rsi_result[100:],
-    name='rsi',  row=2, col=1)
+    name='RSI', row=2, col=1)
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.bb_result[0][100:],
-    name='bollinger_band up',  row=1, col=1)
+    name='bollinger_band up', row=1, col=1)
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.bb_result[1][100:],
@@ -42,24 +76,24 @@ fig.add_scatter(
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.sma_result[20][100:],
-    name='SMA 20',row=1, col=1)
+    name='SMA 20', row=1, col=1)
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.sma_result[50][100:],
-    name='SMA 50',row=1, col=1)
+    name='SMA 50', row=1, col=1)
 fig.add_scatter(
     x=data['open_time'][100:],
     y=indicators.sma_result[100][100:],
-    name='SMA 100',row=1, col=1)
+    name='SMA 100', row=1, col=1)
 
 fig.add_candlestick(x=data['open_time'][100:],
-                                   open=data['open'][100:],
-                                   high=data['high'][100:],
-                                   low=data['low'][100:],
-                                   close=data['close'][100:],
-name='Market price',
-    row=1, col=1
-)
+                    open=data['open'][100:],
+                    high=data['high'][100:],
+                    low=data['low'][100:],
+                    close=data['close'][100:],
+                    name='Market price',
+                    row=1, col=1
+                    )
 fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
 fig.show()
 
